c184dfdddf270c812f9a3e2a3f080470b8d4deef
Anthonini <anthonini@gmail.com>
2018-07-15 20:12:45 -0300

    Created user classes
    - Created User Class
    - Created UserGroup Class
    - Created Permission Class
    - Created Flyway Migration
    
    
    Signed-off-by: Anthonini <anthonini@gmail.com>

---
+++ src/main/java/com/anthonini/brewer/model/User.java
@@ -0,0 +1,145 @@
+package com.anthonini.brewer.model;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "user")
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_user")
+	private Long id;
+	
+	@NotBlank(message = "Nome é obrigatório")
+	private String name;
+	
+	@NotBlank(message = "E-mail é obrigatório")
+	@Email(message = "E-mail inválido")
+	private String email;
+	
+	@NotBlank(message = "Senha é obrigatório")
+	private String password;
+	
+	@NotBlank(message = "Confirmação de senha é obrigatório")
+	@Transient
+	private String passwordConfirmation;
+	
+	private boolean active;
+	
+	@Column(name = "birth_date")
+	private LocalDate birthDate;
+	
+	@NotNull(message = "Selecione pelo menos um grupo")
+	@ManyToMany
+	@JoinTable(name = "user_group_user", joinColumns = @JoinColumn(name = "id_user")
+				,inverseJoinColumns = @JoinColumn(name = "id_user_group"))
+	private List<UserGroup> groups;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public boolean isActive() {
+		return active;
+	}
+	
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+	
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public List<UserGroup> getGroups() {
+		return groups;
+	}
+
+	public void setGroups(List<UserGroup> groups) {
+		this.groups = groups;
+	}
+
+	public String getPasswordConfirmation() {
+		return passwordConfirmation;
+	}
+
+	public void setPasswordConfirmation(String passwordConfirmation) {
+		this.passwordConfirmation = passwordConfirmation;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
