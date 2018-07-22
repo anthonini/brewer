9204d0da48ce35ef9f6a43de7f0781bfa9620d05
Anthonini <anthonini@gmail.com>
2018-07-22 01:20:54 -0300

    Configured csrf
    Signed-off-by: Anthonini <anthonini@gmail.com>

--- src/main/resources/static/javascripts/brewer.js
+++ src/main/resources/static/javascripts/brewer.js
@@ -71,6 +71,22 @@ Brewer.MaskDate = (function() {
 	
 }());
 
+Brewer.Security = (function() {
+	
+	function Security() {
+		this.token = $('input[name=_csrf').val();
+		this.header = $('input[name=_csrf_header]').val();
+	}
+	
+	Security.prototype.enable = function() {
+		$(document).ajaxSend(function(event, jqxhr, settings){
+			jqxhr.setRequestHeader(this.header, this.token);
+		}.bind(this));
+	}
+	
+	return Security;
+}())
+
 $(function(){	
 	var maskNumber = new Brewer.MaskMoney();
 	maskNumber.enable();
@@ -83,4 +99,7 @@ $(function(){
 	
 	var maskDate = new Brewer.MaskDate();
 	maskDate.enable();
+	
+	var security = new Brewer.Security();
+	security.enable();
 });
