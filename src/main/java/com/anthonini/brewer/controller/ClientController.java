c57f1af66b991724832d1b8eab66dd0aa486b0b6
Anthonini <anthonini@gmail.com>
2018-07-22 14:34:50 -0300

    Finished Client search
    Signed-off-by: Anthonini <anthonini@gmail.com>

--- src/main/java/com/anthonini/brewer/controller/ClientController.java
+++ src/main/java/com/anthonini/brewer/controller/ClientController.java
@@ -1,8 +1,11 @@
 package com.anthonini.brewer.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +14,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.anthonini.brewer.controller.page.PageWrapper;
 import com.anthonini.brewer.model.Client;
 import com.anthonini.brewer.model.PersonType;
+import com.anthonini.brewer.repository.ClientRepository;
 import com.anthonini.brewer.repository.StateRepository;
+import com.anthonini.brewer.repository.filter.ClientFilter;
 import com.anthonini.brewer.service.ClientService;
 import com.anthonini.brewer.service.exception.AlreadyRegisteredClientCpfCnpjException;
 
@@ -26,6 +32,9 @@ public class ClientController {
 	
 	@Autowired
 	private ClientService clientService;
+	
+	@Autowired
+	private ClientRepository clientRepository;
 
 	@GetMapping("/new")
 	public ModelAndView form(Client client) {
@@ -52,5 +61,16 @@ public class ClientController {
 			bindingResult.rejectValue("cpfCnpj", e.getMessage(), e.getMessage());
 			return form(client);
 		}
+	}
+	
+	@GetMapping
+	public ModelAndView list(ClientFilter clientFilter, BindingResult bindingResult,
+			@PageableDefault(size = 2) Pageable pageable, HttpServletRequest httpServletRequest) {
+		ModelAndView mv = new ModelAndView("client/list");
+		
+		PageWrapper<Client> pageWrapper = new PageWrapper<>(clientRepository.filter(clientFilter, pageable), httpServletRequest);
+		mv.addObject("page", pageWrapper);
+		
+		return mv;
 	}
 }
--- src/main/java/com/anthonini/brewer/repository/filter/ClientFilter.java
+++ src/main/java/com/anthonini/brewer/repository/filter/ClientFilter.java
@@ -4,15 +4,19 @@ public class ClientFilter {
 
 	private String name;
 	private String cpfCnpj;
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public String getCpfCnpj() {
 		return cpfCnpj;
 	}
+	
 	public void setCpfCnpj(String cpfCnpj) {
 		this.cpfCnpj = cpfCnpj;
 	}
