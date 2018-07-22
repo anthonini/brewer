62da47cf2fb5153b9dfebcbdc35ec4fcda93bd84
Anthonini <anthonini@gmail.com>
2018-07-22 13:44:47 -0300

    Created error page 500
    Signed-off-by: Anthonini <anthonini@gmail.com>

--- src/main/java/com/anthonini/brewer/controller/ErrorsController.java
+++ src/main/java/com/anthonini/brewer/controller/ErrorsController.java
@@ -2,12 +2,18 @@ package com.anthonini.brewer.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ErrorsController {
 
 	@GetMapping("/404")
-	public String paginaNaoEncontrada() {
+	public String pageNotFound() {
 		return "404";
+	}
+	
+	@RequestMapping("/500")
+	public String serverInternalError() {
+		return "500";
 	}
 }
