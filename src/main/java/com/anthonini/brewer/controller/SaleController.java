98787a2be9a538c48b76740a70ff9c3f9f0dafe7
Anthonini <anthonini@gmail.com>
2020-06-04 01:47:05 -0300

    Migrating to Spring Boot 2.
    - Moved error pages to Spring Boot default.
    - Ajusted error pages config.
    - Removed exception handling from spring security.
    - Removed forward to 403 erro when a user try to cancel a sale that he
    was not registered by him. Just let throw AccessDeniedException.
    
    
    
    Signed-off-by: Anthonini <anthonini@gmail.com>

--- src/main/java/com/anthonini/brewer/controller/SaleController.java
+++ src/main/java/com/anthonini/brewer/controller/SaleController.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
@@ -121,11 +120,7 @@ public class SaleController {
 	
 	@PostMapping(value = "/new", params = "cancel")
 	public ModelAndView cancel(Sale sale, BindingResult bindingResult, RedirectAttributes redirectAttributes, @AuthenticationPrincipal SystemUser systemUser) {
-		try {
-			saleService.cancel(sale);
-		} catch (AccessDeniedException e) {
-			return new ModelAndView("/403");
-		}
+		saleService.cancel(sale);
 		
 		redirectAttributes.addFlashAttribute("successMessage", "Venda cancelada com sucesso");
 		return new ModelAndView("redirect:/sale/"+sale.getId());
